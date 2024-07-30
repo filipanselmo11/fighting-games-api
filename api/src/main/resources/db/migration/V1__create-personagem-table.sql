9e1ba3ccd4ae08b2be928898725b14e18ee2a2ca
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2024-07-30 11:00:12 -0400

    feat: Arrumando o db

---
+++ api/src/main/resources/db/migration/V1__create-personagem-table.sql
@@ -0,0 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE personagem(
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    img VARCHAR(100) NOT NULL,
+    jogo VARCHAR(255) NOT NULL
+);
