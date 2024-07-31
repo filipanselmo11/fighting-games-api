fdca98d63adbf59879ca628cbbe700a706b9c6ea
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2024-07-31 10:27:32 -0400

    feat: Atualizando configs do cors

--- api/src/main/java/com/fighting_games/api/ApiApplication.java
+++ api/src/main/java/com/fighting_games/api/ApiApplication.java
@@ -2,12 +2,33 @@ package com.fighting_games.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@Configuration
+	public class WebConfig implements WebMvcConfigurer {
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedOrigins("http://localhost:4200") // Corrigido para definir a origem permitida
+							.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Corrigido para definir os métodos permitidos
+							.allowedHeaders("*")
+							.allowCredentials(true);
+				}
+			};
+		}
 	}
 
 }
